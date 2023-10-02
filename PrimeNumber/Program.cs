a7c7ec39aa5efb97a28d8a74f954fa918d2b85ec
Amod Salankar <amod.salankar@nitorinfotech.com>
2023-10-02 15:57:57 +0530

    Add project files.

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+static bool IsPrime(int number)
+{
+    if (number <= 1)
+    {
+        return false;
+    }
+    for (int i = 2; i <= Math.Sqrt(number); i++)
+
+    {
+
+        if (number % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+Console.WriteLine(IsPrime(11));
